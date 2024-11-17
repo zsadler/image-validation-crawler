f71e8376fa31b429d752cf1a3a7961f756c95103
Zoe Sadler-Jasmin <zsadler@users.noreply.github.com>
2024-11-16 21:24:26 -0500

    initial shadcn-vue and tailwind setup (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css'
+import './assets/tailwind.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from 'node:url';
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
 export default defineConfig({
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()]
+        }
+    },
     plugins: [vue()],
     resolve: {
         alias: {
